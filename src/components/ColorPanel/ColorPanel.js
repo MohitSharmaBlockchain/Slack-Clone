f27b588a3f0e21abbda87b6c425a5e806c5f8fe5
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-04 23:58:34 +0530

    Add ColorPanel Component

---
+++ src/components/ColorPanel/ColorPanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ColorPanel extends React.Component {
+    render() {
+        return (
+            <p>ColorPanel</p>
+        )
+    }
+}
+
+export default ColorPanel
