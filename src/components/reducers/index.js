2d4c8c05b57b00f90da89e987c4a1f156acfe934
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-05 22:19:31 +0530

    Added Reducer for Add Channel

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -5,6 +5,7 @@ const initialUserState = {
     currentUser: null,
     isLoading: true
 }
+
 const user_reducer = (state = initialUserState, action) => {
     switch (action.type) {
         case actionTypes.SET_USER:
@@ -12,13 +13,36 @@ const user_reducer = (state = initialUserState, action) => {
                 currentUser: action.payload.currentUser,
                 isLoading: false
             }
+        case actionTypes.CLEAR_USER:
+            return {
+                ...state,
+                isLoading: false
+            }
+        default:
+            return state
+    }
+}
+
+const initialChannelState = {
+    currentChannel: null,
+    isLoading: true
+}
+
+const channel_reducer = (state = initialChannelState, action) => {
+    switch (action.type) {
+        case actionTypes.SET_CURRENT_CHANNEL:
+            return {
+                ...state,
+                currentChannel: action.payload.currentChannel
+            }
         default:
             return state
     }
 }
 
 const rootReducer = combineReducers({
-    user: user_reducer
+    user: user_reducer,
+    channel: channel_reducer
 })
 
 export default rootReducer
