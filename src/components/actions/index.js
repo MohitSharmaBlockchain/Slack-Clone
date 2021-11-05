fdb06f01854c8a4cfb0f80a39fe9d333da5e25b0
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-05 22:18:52 +0530

    Added Actions for Add Channel

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -8,3 +8,18 @@ export const setUser = (user) => {
         }
     }
 }
+
+export const setCurrentChannel = (channels) => {
+    return {
+        type: actionTypes.SET_CURRENT_CHANNEL,
+        payload: {
+            currentChannel: channels
+        }
+    }
+}
+
+export const ClearUser = () => {
+    return {
+        type: actionTypes.CLEAR_USER,
+    }
+}
