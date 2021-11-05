104f1b49391d3a5596177456ac35575324342aa2
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-05 22:21:43 +0530

    Created Chat Base

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import SidePanel from './SidePanel/SidePanel';
+import ColorPanel from './ColorPanel/ColorPanel';
+import MetaPanel from './MetaPanel/MetaPanel';
+import Messages from './Messages/Messages';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        SLACK
+      <div className="AppGrid">
+        <SidePanel currentUser={this.props.currentUser}/>
+        <Messages/>
+        <MetaPanel/>
+        <ColorPanel/>
       </div>
     );
   }
 }
 
-export default App;
+export default connect((state) => {
+  return {
+    currentUser: state.user.currentUser
+  }
+})(App);
