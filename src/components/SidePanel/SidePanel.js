b5bb10b5ce8519bf720fb7fc30059b18deb51b14
Mohit Sharma <vipersharma.88@gmail.com>
2021-11-05 22:21:02 +0530

    Completed SidePanel

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,12 +1,14 @@
 import React from "react";
 import UserPanel from "./UserPanel";
+import Channels from "./Channels";
 
 class SidePanel extends React.Component {
     render() {
         return (
             <div>
             <p>SidePanel</p>
-            <UserPanel/>
+            <UserPanel currentUser = {this.props.currentUser}/>
+            <Channels currentUser = {this.props.currentUser}/>
             </div>
         )
     }
--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,14 +1,23 @@
+import { signOut } from "@firebase/auth";
 import React from "react";
+import { auth } from "../../firebase";
 
 class UserPanel extends React.Component {
+    state = {
+        user: this.props.currentUser
+    }
     handleSignOut = () => {
-
+        signOut(auth)
     }
     render() {
+        console.log(this.state.user)
         return (
             <div>
+                <img src={this.state.user.photoURL}/>
+                <p>Signed In as {this.state.user.displayName}</p>
                 <button>Change Avatar</button>
             <button onClick={this.handleSignOut}>Sign Out</button>
+            <hr/>
             </div>
         )
     }
